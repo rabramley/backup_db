799e2936cf2e459692af192c39b9f83da3c012b7
root <root@localhost.localdomain>
2025-01-05 10:11:04 +0000

    Backup dem MySQL Databases

--- backup_dbs.py
+++ backup_dbs.py
@@ -2,6 +2,7 @@
 import tomllib
 import subprocess
 from pathlib import Path
+from datetime import datetime
 
 
 def get_config():
@@ -39,29 +40,66 @@ def mysql_databases(config):
     return [s for s in result.stdout.splitlines() if s not in not_dbs]
 
 
-def backup_mysql(config):
+def backup_mysql_database(db: str, backup_dir: Path):
+    print(f"Backing up {db} into '{backup_dir}'")
+
+    backup_dir = backup_dir / db
+    backup_dir.mkdir(parents=True, exist_ok=True)
+
+    filename = f"{datetime.now().strftime('%Y-%m-%d-%H-%M-%S')}.sql"
+
+    filepath = backup_dir / filename
+
+    result = subprocess.run(
+        [
+            "mysqldump", 
+            f"--defaults-file={MYSQL_CONFIG_FILENAME}", 
+            "--force",
+            "--opt",
+            "--databases",
+            "--events",
+            "--routines",
+            "--triggers",
+            db,
+        ],
+        capture_output=True,
+        text=True)
+
+    if result.returncode:
+        print(result.stderr)
+        return []
+
+    with open(filepath, "w") as f:
+        f.writelines(result.stdout)
+
+
+def backup_mysql(config: dict, backup_dir: Path):
     print(f"Backing up MySQL server '{config['name']}'")
     mysql_create_config(config)
+    backup_dir = backup_dir / config["name"]
+    print(backup_dir)
 
     for db in mysql_databases(config):
-        print(db)
+        backup_mysql_database(db, backup_dir)
 
     MYSQL_CONFIG_FILENAME.unlink()
 
 
-def backup_postgres(config):
+def backup_postgres(config: dict, backup_dir: Path):
     print(f"Backing up Postgres server '{config['name']}'")
 
 
 def main():
     config = get_config()
 
+    backup_dir = Path(config["backup_dir"])
+
     for s in config.get('servers', []):
         match t := s.get('type', ''):
             case 'mysql':
-                backup_mysql(s)
+                backup_mysql(s, backup_dir)
             case "postgres":
-                backup_postgres(s)
+                backup_postgres(s, backup_dir)
             case _:
                 print(f"I don't know what a type of '{t}' is")
 
